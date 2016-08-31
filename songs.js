1680f35992b8d6478369881771c02bb904d59203
Dustin Reed <dmreedy1@gmail.com>
2016-08-31 14:20:02 -0500

    finished add music form

--- songs.js
+++ songs.js
@@ -1,10 +1,10 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[0] = "Legs > by Z*ZTop on the album Eliminator";
+songs[1] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[2] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[3] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[4] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 songs.unshift("Granddaddy's Gun > by Aaron Lewis on the album The Road");
 songs.push("Straight Outta Cold Beer > by Blake Shelton on the album If I'm Honest");
@@ -12,13 +12,46 @@ songs.push("Straight Outta Cold Beer > by Blake Shelton on the album If I'm Hone
 var finalStr = "";
 var content = document.getElementById("content");
 
-for (x in songs){
-  var songInfo = songs[x]
-  songInfo = songInfo.replace(">", "-");
-  songInfo = songInfo.replace("@","");
-  songInfo = songInfo.replace("!","");
-  songInfo = songInfo.replace("*","");
-  songInfo = songInfo.replace("(","");
-
-  content.innerHTML += "<p>" + songInfo + "</p";
+function renderSongs() {
+  let songList = songs.map((song)=>{
+    song.replace(">", "-");
+    song.replace("@","");
+    song.replace("!","");
+    song.replace("*","");
+    song.replace("(","");
+    return "<p>" + song + "</p>";
+  }).join("");
+  content.innerHTML = songList;
 }
+
+var addMusicLink = document.getElementById('add-music-link');
+var viewMusicLink = document.getElementById('view-music-link');
+
+var addMusicView = document.getElementById('add-music-view');
+var listMusicView = document.getElementById('list-music-view');
+
+addMusicLink.addEventListener('click', function (e) {
+  e.preventDefault();
+  addMusicView.classList.remove('hidden');
+  listMusicView.classList.add('hidden');
+});
+
+viewMusicLink.addEventListener('click', function (e) {
+  e.preventDefault();
+  addMusicView.classList.add('hidden');
+  listMusicView.classList.remove('hidden');
+});
+
+function addSong() {
+  let songName = document.getElementById('song-name').value;
+  let songArtist = document.getElementById('song-artist').value;
+  let songAblum = document.getElementById('song-album').value;
+
+  songs.push(`${songName} > by ${songArtist} on the album ${songAblum}`);
+  console.log('songs', songs);
+  renderSongs();
+}
+
+document.getElementById('addBtn').addEventListener('click', addSong);
+
+renderSongs();
